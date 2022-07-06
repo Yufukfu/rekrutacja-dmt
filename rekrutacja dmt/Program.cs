18c73ed55c628f92d18834de16b75fa566ab84db
Yufukfu <yufukfu@gmail.com>
2022-07-06 19:47:11 +0200

    add caller to Rekrutacja.Main()

--- rekrutacja dmt/Program.cs
+++ rekrutacja dmt/Program.cs
@@ -13,43 +13,12 @@ using System.Text.RegularExpressions;
 namespace rekrutacja_dmt
 
 {
-    internal class Program
+    internal class Programs
     {
-
         static void Main()
         {
-            string filename1 = "CardsList.json";
-            string filename2 = "Ranges.json";
-            string schemaname1 = "CardsListSchema.json";
-            string schemaname2 = "RangesSchema.json";
-
-            var cardsList = new CardsList();
-            cardsList.ReadFiles(filename1, schemaname1);
-            cardsList.IsValid();
-            cardsList.Deserialize();
-
-            var rangesList = new RangesList();
-            rangesList.ReadFiles(filename2, schemaname2);
-            rangesList.IsValid();
-            rangesList.Deserialize();
-
-            foreach(Range range in rangesList.Ranges)
-            {
-                range.IsValid();
-            }
-            rangesList.CheckOverlap();
-
-            foreach(Card card in cardsList.Cards)
-            {
-                card.IsPANvalid();
-                card.CheckExpirationDate();
-                card.CheckVerificationMethod();
-                card.AssessCardName(rangesList);
-            }
-
-
-            Console.ReadLine();
-
+            var rekrutacja = new Rekrutacja();
+            string LogString = rekrutacja.Main();
         }
     }
 }
---
+++ rekrutacja dmt/Rekrutacja.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Schema;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using System.Text.RegularExpressions;
+
+namespace rekrutacja_dmt
+
+{
+    internal class Rekrutacja
+    {
+        public string Main(string filename1 = "CardsList.json", string filename2 = "Ranges.json")
+        {
+            string schemaname1 = "CardsListSchema.json";
+            string schemaname2 = "RangesSchema.json";
+
+            var rangesList = new RangesList();
+            rangesList.ReadFiles(filename2, schemaname2);
+            if (rangesList.IsValid())
+            {
+                rangesList.Deserialize();
+                rangesList.CheckOverlap();
+            }
+            var cardsList = new CardsList();
+            cardsList.ReadFiles(filename1, schemaname1);
+            if (cardsList.IsValid())
+            {
+                cardsList.Deserialize();
+                cardsList.CheckCards(rangesList);
+            }
+            return Logger.LogString.ToString();
+        }
+    }
+}
